d092e22ffc2e85285ecfdb2a8a0bb710e355b642
josmera <josmera@gmail.com>
2021-05-09 15:20:05 -0500

    modificacion esquemaMySQL

--- esquemaMySQL.tcl
+++ esquemaMySQL.tcl
@@ -4,9 +4,9 @@ dbset db mysql
 diset connection mysql_host 127.0.0.1
 diset connection mysql_port 3306
 diset connection mysql_socket /var/run/mysqld/mysqld.sock
-diset tpcc mysql_count_ware 800
+diset tpcc mysql_count_ware 10
 diset tpcc mysql_partition true
-diset tpcc mysql_num_vu 64
+diset tpcc mysql_num_vu 8
 diset tpcc mysql_storage_engine innodb
 print dict
 buildschema
