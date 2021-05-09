d3d2afc18a0118a73ddc107ba816e588ffaeb766
josmera <josmera@gmail.com>
2021-05-09 16:08:52 -0500

    script prueba

--- prueba.sh
+++ prueba.sh
@@ -3,8 +3,10 @@
 cd ../HammerDB-4.1
 sudo bash mysql -h "localhost" -u "root" "-pmysql" DROP DATABASE tpcc;
 #bash mysql DROP DATABASE tpcc;
-echo "Construyendo esquema, espere por favor..."
-bash hammerdbcli auto esquemaMySQL.tcl > log.txt
-echo "Esquema construido"
+#echo "Construyendo esquema, espere por favor..."
+#mkdir ../HammerDB_MySQL
+#bash hammerdbcli auto esquemaMySQL.tcl > ../HammerDB_MySQL/log.txt
+#echo "Esquema construido"
 echo "Empezando prueba"
-bash hammerdbcli auto pruebaMySQL.tcl > ejecucion.txt
+bash hammerdbcli auto pruebaMySQL.tcl > ../HammerDB_MySQL/ejecucion.txt
+echo "Fin prueba, consulte los resultados en ../HammerDB_MySQL/ejecucion.txt"
